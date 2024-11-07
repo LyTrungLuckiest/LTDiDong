7e5349f2a64fa1495435a8ad561951ca800a0877
HaTien3225 <Hatien3225@gmail.com>
2024-11-07 21:14:04 +0700

    sua loi bootom bar và them quãng cáo

--- LTDiDong/app/src/main/java/com/example/btlon/MainActivity.java
+++ LTDiDong/app/src/main/java/com/example/btlon/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.btlon;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
+
+import com.bumptech.glide.Glide;
+
+import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    ViewFlipper  viewFlipper;
+    RecyclerView recyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MakeToast();
+        ActionViewFlipper();
+
     }
+
+    private void MakeToast() {
+        viewFlipper= findViewById(R.id.viewFlipper);
+        recyclerView=findViewById(R.id.recyclerview);
+    }
+    private void ActionViewFlipper(){
+        List<String> mangquangcao=new ArrayList<>();
+        mangquangcao.add("https://www.bigc.vn/files/a-31-08-2023-11-41-07/21-31-01-si-u-h-i-tr-i-c-y-1080go.jpg");
+        mangquangcao.add("https://www.bigc.vn/files/banners/2022/feb/tra-i-ca-y-giao-mu-a-1080x540-bigc.jpg");
+        mangquangcao.add("https://www.bigc.vn/files/banners/new-node-31-05-2023-11-41-17/mega-mid-june-15-06-2023-16-56-56/1080-g-15-28-06-fruit-festival.jpg");
+        for (int i=0;i<mangquangcao.size();i++){
+            ImageView imageView=new ImageView(getApplicationContext());
+            Glide.with(getApplicationContext()).load(mangquangcao.get(i)).into(imageView);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            viewFlipper.addView(imageView);
+
+        }
+        viewFlipper.setFlipInterval(3000);
+        viewFlipper.setAutoStart(true);
+        Animation slide_in= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_in_right);
+        Animation slide_out=AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_out_right);
+        viewFlipper.setInAnimation(slide_in);
+        viewFlipper.setOutAnimation(slide_out);
+    }
+
 }
